8b6428a5a9b305c22499b60527cca1f50c74385a
TonyaOs <tonya.mipt@gmail.com>
2021-10-29 11:43:29 +0300

    add content for task 3&6

--- Homework_2/HW2_3.py
+++ Homework_2/HW2_3.py
@@ -35,3 +35,14 @@ if month > 12 or month < 1 :
 else:
     print (month_dict[month])
 
+#еще один способ
+while True:
+    user_month = input('Введите номер от 1 до 12: ')
+    if user_month.isdigit() and 1 <= int(user_month) <= 12:
+        season_1 = ['зима', 'весна', 'лето', 'осень', 'зима' ]
+        season_2 = {0: 'зима', 1: 'весна', 2: 'лето', 3: 'осень', 4: 'зима'}
+        print(f'Список - {season_1[int(user_month)//3]}\nСловарь - {season_2[int(user_month)//3]}')
+        break
+    else: print('Error')
+
+
--- Homework_2/HW2_6.py
+++ Homework_2/HW2_6.py
@@ -49,3 +49,31 @@ for i in range(len(list_products)):
     i += 1
 
 print(f'Аналитика: {dict_products}')
+
+# второй вариант
+goods = []
+features = {'название': '',
+            'цена': '',
+            'кол-во': '',
+            'ед. измерения': ''}
+analytics = {'название': [],
+            'цена': [],
+            'кол-во': [],
+            'ед. измерения': []}
+
+num = 0
+while True:
+    if input('Для выхода из приложения нажмите Q, для продолжения Enter: ').upper() =='Q'\
+            :
+        break
+    num += 1
+    for f in features.keys():
+        prop = input(f'Введите значение свойства {f}: ')
+        features[f] = int(prop) if f in ('цена', 'кол-во') else prop
+        analytics[f].append(features[f])
+    goods.append((num, features.copy()))
+    print(f'\nСтруктура товаров\n {goods}')
+    print(f'\nТекущая аналитика по товарам: \n {"*" * 30}')
+    for key, value in analytics.items():
+        print(f'{key[:25]:>30}: {value}')
+    print('*' * 30)
